4d9e2a8742e9334728fda8970ea0febfcc1c82f2
Gerald Dachs <gda@dachsweb.de>
2019-12-26 21:25:26 +0100

    Initial Z offset for probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -25, 0, 0 } // estimated
+#define NOZZLE_TO_PROBE_OFFSET { -25, 0, -2 } // estimated
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
